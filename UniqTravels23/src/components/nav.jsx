fbbb082c7d596f3e6fdb0c7e3f9aa6fa7f6e9547
MattyDa1 <mso02468@gmail.com>
2023-10-24 01:46:48 +0200

    MU 1.1

--- UniqTravels23/src/components/nav.jsx
+++ UniqTravels23/src/components/nav.jsx
@@ -5,11 +5,25 @@ import '../styles/matty.css'
 export default function Nav() {
     return (
         <nav>
-            <NavLink to='/' >Home</NavLink>
-            <NavLink to='/booking' >Booking</NavLink>
-            <NavLink to='/map' >Map</NavLink>
-            <NavLink to='/planner' >Planner</NavLink>
-            <NavLink to='/user' >User</NavLink>
+            <aside className="border nav_left"></aside>
+            <section className="nav">
+                <div>
+                    <NavLink to='/' className={'nav_link'}><p className="nav_link">Home</p></NavLink>                
+                </div>
+                <div>
+                    <NavLink to='/booking' className={'nav_link'}><p className="nav_link">Booking</p></NavLink>
+                </div>
+                <div>
+                    <NavLink to='/map' className={'nav_link'}><p className="nav_link">Map</p></NavLink>
+                </div>
+                <div>
+                    <NavLink to='/planner' className={'nav_link'}><p className="nav_link">Planner</p></NavLink>
+                </div>
+                <div>
+                    <NavLink to='/user' className={'nav_link'}><p className="nav_link">User</p></NavLink>
+                </div>
+            </section>
+            <aside className="border nav_right"></aside>
         </nav>
     );
 }
