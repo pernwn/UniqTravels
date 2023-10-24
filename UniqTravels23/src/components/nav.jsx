bd3b17589fc609433cf4ec1338b268f557f5640a
MattyDa1 <mso02468@gmail.com>
2023-10-24 11:41:28 +0200

    M.U. 1.3

--- UniqTravels23/src/components/nav.jsx
+++ UniqTravels23/src/components/nav.jsx
@@ -15,42 +15,40 @@ import Map from '../assets/pictures/Map.png'
 import User from '../assets/pictures/User.png'
 
 //Mangler: Cirkel der flytter sig alt efter siden og gradient farve
-//Mangler: funktion af billeder
 //Mangler: få Root til at virke
-//noter til koden under (kan ikke //, /**/ eller <!---->, hvordan gør man?)
+//noter til koden under {/*KOMMENTAR*/}
 export default function Nav() {
     return (
         <nav>
             <aside className="nav_border nav_left"></aside>
             <section className="nav">
                 <div>
-                    
                     <NavLink to='/' className={'navlink'}>
-                    <img src="../assets/pictures/Home.png" alt="Home" className="nav_img"/>
+                    <img src={Home} alt="Home" className="nav_img"/>
                         <p>Home</p>
                     </NavLink>                
                 </div>
                 <div>
                     <NavLink to='/booking' className={'navlink'}>
-                        <img src="../assets/pictures/Booking.png" alt="Booking" className="nav_img"/>
+                        <img src={Booking} alt="Booking" className="nav_img"/>
                         <p>Booking</p>
                     </NavLink>
                 </div>
                 <div>
                     <NavLink to='/map' className={'navlink'}>
-                        <img src="../assets/pictures/Map.png" alt="Map" className="nav_img"/>
+                        <img src={Map} alt="Map" className="nav_img"/>
                         <p>Map</p>
                     </NavLink>
                 </div>
                 <div>
                     <NavLink to='/planner' className={'navlink'}>
-                        <img src="../assets/pictures/Planner.png" alt="Planner" className="nav_img"/>
+                        <img src={Planner} alt="Planner" className="nav_img"/>
                         <p>Planner</p>
                     </NavLink>
                 </div>
                 <div>
                     <NavLink to='/user' className={'navlink'}>
-                        <img src="../assets/pictures/User.png" alt="User" className="nav_img"/>
+                        <img src={User} alt="User" className="nav_img"/>
                         <p>User</p>
                     </NavLink>
                 </div>
