0e7839ab8c5c051d8cf30a710302e63322e71548
Rinamunk <eaarimun@students.eaaa.dk>
2023-10-25 09:20:02 +0200

    booking

--- UniqTravels23/src/pages/booking.jsx
+++ UniqTravels23/src/pages/booking.jsx
@@ -1,8 +1,18 @@
 //HUSK AT SKRIVE KOMMENTARER TIL NÆRMEST ALT – forklaring samt hvad koden gør
+import { customTheme } from '../themes/themes';
+import { useTheme } from '@mui/material/styles';
+
+import { CssBaseline, ThemeProvider, Typography } from '@mui/material';
+
+
+
 export default function Booking() {
+    const outerTheme = useTheme();
     return (
-        <>
-            
-        </>
+        <ThemeProvider theme={customTheme(outerTheme)}>
+            <Typography></Typography>
+
+        </ThemeProvider>   
+    
     );
 }
