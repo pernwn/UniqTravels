2bbe6aa3db85897714dad56c0f92a82f53b893d6
Rinamunk <eaarimun@students.eaaa.dk>
2023-10-25 09:09:48 +0200

    flere kommentarer

--- UniqTravels23/src/pages/login.jsx
+++ UniqTravels23/src/pages/login.jsx
@@ -8,22 +8,28 @@ import { Button, CssBaseline, ThemeProvider, Typography, TextField } from "@mui/
 import { useState } from "react";
 
 export default function Login() {
+   // Bruger React's 'useTheme' hook til at hente det aktuelle tema
   const outerTheme = useTheme();
+  // Opretter to tilstande (states) for email og password
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   // vi bruger reacts "useState" hook og sætter password og email som en tom string('')
   // vi inkluderer også setPassword og setEmail. Det betyder at senere kan vi opdatere dem ved at skrive 'setPassword(nyværdi)'
 
+  // Opretter en funktion kaldet 'handleLogin', der udføres, når 'Login' knappen klikkes
   const handleLogin = () => {
     if (email === "user@example.com" && password === "password") {
+      // Hvis email og password matcher, vises en velkomstbesked
       alert("welcome!");
     } else {
+       // Hvis email og password ikke matcher, vises en fejlbesked
       alert("lol noob");
     }
   };
   // i denne sætning if (email === 'user@example.com' && password === 'password') starter vi en 'if' sætning for at undersøge om email og password matcher
   return (
+    // Anvender det brugerdefinerede tema på komponenten
     <ThemeProvider theme={customTheme(outerTheme)}>
       <section>
         <div>
