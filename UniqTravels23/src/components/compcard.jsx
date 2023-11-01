d3c9f8a8dfd1456ee61aadd0b1b7530a10053e50
Rinamunk <eaarimun@students.eaaa.dk>
2023-11-01 22:37:01 +0100

    heyy

--- UniqTravels23/src/components/compcard.jsx
+++ UniqTravels23/src/components/compcard.jsx
@@ -12,15 +12,19 @@ import MoreVert from '@mui/icons-material/MoreVert';
 import { NavLink } from 'react-router-dom';
 
 export default function ComplexCard(props) {
+    //Ved at bruge props kan vi overføre data eller egenskaber fra det, der kalder ComplexCard-komponenten, til ComplexCard-komponenten selv. 
+    //Dette gør det muligt at tilpasse det generelle udseende og indhold i hvert kort.
+    //Dette betyder at vi kan oprette flere kort med forskellige label, title, subheader, og image, uden at skulle skrive en separat komponent til hvert kort.
+
+
     const cardContentStyle = {
-        maxHeight: '200px', // Set the max height for the card content
-        overflow: 'hidden', // Hide the overflow
-        textOverflow: 'ellipsis', // Add ellipsis for the text overflow
+        maxHeight: '200px', // Sætter en maks højde for vores kort
+        overflow: 'hidden', // Gemmer overflow væk
+        textOverflow: 'ellipsis', // Tilføjer en ellipse til overflow
         
       };
 return(
     <Card className="compcard"
-    
     >
         <CardHeader
         avatar={ 
@@ -34,7 +38,6 @@ return(
 
             </IconButton>
         }
-            
             title={props.title}
             subheader={props.subheader} 
         />
@@ -45,8 +48,11 @@ return(
         >
              
         </CardMedia>
-        <CardContent sx={cardContentStyle}>
-            <Typography variant="body2" color="text.secondary" //Jeg vil gerne få den her til at klappe sig lidt sammen så man kun kan se starten af teksten, kigger på det senere
+        <CardContent sx={cardContentStyle}> 
+        {/*Her bruger vi den const der blev defineret i starten 
+           Den sørger for at når kortet har ramt sin max højde så bliver det overskydende tekst skåret fra. 
+           Det sikrer at alle kort står lækkert og ensformigt med minimal forstyrrelse til brugeren  */}
+            <Typography variant="body2" color="text.secondary" 
             > 
                {props.body}
             </Typography>
