68703cfc721e159c93389eeeb54d92f001a9ba26
pernwn <pernillepjohansen@hotmail.com>
2023-11-07 11:09:57 +0100

    ? hello

---
+++ UniqTravels23/src/App.jsx
@@ -0,0 +1,67 @@
+
+import { Outlet, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Nav from './components/nav'
+import HomePage from './pages/homepage'
+import Planner from './pages/planner'  //TODO: denne laver vi hvis vi har overskud
+
+import User from './pages/user'
+import Booking from './pages/booking'
+//import Map from './pages/map'
+
+import SignUp from './pages/signup'
+import Login from './pages/login'
+
+import Error from './pages/404'
+
+//import af separat theme file
+import { customTheme } from './themes/themes'
+import { useTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+
+
+function App() {
+
+  //Oprettelse af variabel for at bruge useTheme
+  const outerTheme = useTheme();
+  return (
+    <ThemeProvider theme={customTheme(outerTheme)}>
+      <CssBaseline enableColorScheme />
+
+      <Routes>
+
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+<<<<<<< HEAD
+
+    </Routes>
+    </main>
+)
+=======
+        <Route element={
+          <>
+            <Nav />
+            <Outlet /*Outlet bruges her så navigationen ikke bliver vist på sign up og login */ />
+          </>
+        }
+        >
+          <Route path="/" element={<HomePage />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/planner" element={<Planner />} /> {/*TODO: evt kan vi kalde denne for inspiration eller community hvor der kun er reviews af steder, appen osv – evt kan man skrive kommentarer til andre brugere? */}
+          <Route path="/map" element={<Error />} />
+          <Route path="/user" element={<User />} />
+
+          <Route path="*" element={<Error />} />
+
+        </Route>
+
+
+
+      </Routes>
+    </ThemeProvider>
+  )
+>>>>>>> 38c93cf6db78fe7a20d0fbc41ff147dc20cf00da
+}
+
+export default App
---
+++ UniqTravels23/src/pages/map.jsx
@@ -0,0 +1,44 @@
+/*import { Typography } from "@mui/material";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { useMemo} from "react";
+
+import '../styles/vic.css';*/
+
+export default function Map(){
+    /*
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: "AIzaSyDg-KfkR5tDAyRh6lHkjXm8dEpFj06VkT0",
+    });
+    const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), [])
+*/
+    return (
+
+        <><h1>hello</h1></>
+
+    )
+}
+
+
+
+/*        <>
+        
+           <Typography variant="h3">
+                this page is for the map thing with pins and stuff
+            </Typography>
+
+            <div className="gMap">
+                {!isLoaded ? (
+                    <Typography variant="h2">Loading...</Typography>
+                ) : (
+                    <GoogleMap
+                        mapContainerClassName="map-container"
+                        center={center}
+                        zoom={10}
+                    >
+                        <Marker position={{ lat: 18.52043, lng: 73.856743 }} />
+                    </GoogleMap>
+                )}
+
+            </div>
+
+                </>*/
